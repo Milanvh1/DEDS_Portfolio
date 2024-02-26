369fb796ad234ef4a5b04e245076b5d7687bddcd
milan <21104875@student.hhs.nl>
2024-02-26 13:00:49 +0100

    fixes

---
+++ Practicum_3.2_Opdrachten.py
@@ -0,0 +1,9 @@
+
+#Opdracht na HC 2
+#https://www.geeksforgeeks.org/python-data-structures/
+#Bestudeer de link hierboven om bekend te raken met de datastructuren van Python
+#Kies ten minste drie van de datastructuren van python uit
+#Zoek online op per datastructuur die je gekozen hebt hoe deze wordt gebruikt in een bekend algoritme en onderzoek (zoek op) daarvan de complexiteit (big oh). Zorg ervoor dat je de big oh kunt onderbouwen.
+#Schrijf een Python programma waarin je de datastructuren gebruikt om data in op te slaan en weer op te halen. Hiervoor mag je een bekend algoritme gebruiken, maar dat hoeft niet.
+#Schrijf je bevindingen op in je portfolio. Onderbouw de big oh.
+#Gebruik geen chatgpt. Je mag natuurlijk wel je docent vragen stellen ;)
