3485003ab14d8c724d89bc180ffdf58235b014ed
milan <21104875@student.hhs.nl>
2024-02-27 02:13:29 +0100

    helft van de opdrachten afgemaakt

--- Practicum_3.1_Opdrachten.py
+++ Practicum_3.1_Opdrachten.py
@@ -4,3 +4,5 @@
 #Zoek op internet naar ten minste drie verschillende sorteeralgoritmes die je wilt gebruiken om de integer arrays mee te sorteren.
 #Implementer je gekozen sorteeralgoritmes om de integer arrays mee te sorteren. Maak voor elk sorteeralgoritme een nieuwe methode aan die een integer array als parameter heeft en de integer array gesorteerd returnt.
 #Kudos als je een moeilijk, super efficient of creatief algoritme kunt vinden óf zelfs bedenken ;)
+
+#gebruik github copilot
